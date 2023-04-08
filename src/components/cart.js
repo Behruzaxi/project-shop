d74fbd4418fec1a1a994a65c17afb7325a54cfbd
Behruzaxi <ebehruz846@gmail.com>
2023-04-08 16:21:37 +0500

    fds

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,3 +1,55 @@
+// import React, { useState, useEffect } from "react";
+// import "../styles/cart.css";
+
+// const Cart = ({ cart, setCart, handleChange }) => {
+//   const [price, setPrice] = useState(0);
+
+//   const handleRemove = (id) => {
+//     const arr = cart.filter((item) => item.id !== id);
+//     setCart(arr);
+//     handlePrice();
+//   };
+
+//   const handlePrice = () => {
+//     let ans = 0;
+//     cart.map((item) => (ans += item.amount * item.price));
+//     setPrice(ans);
+//   };
+
+//   useEffect(() => {
+//     handlePrice();
+//   });
+
+//   return (
+//     <article>
+//       {cart.map((item) => (
+//         <div className="cart_box" key={item.id}>
+//           <div className="cart_img"> 
+//             <img src={item.img} alt="" />
+//             <p>{item.title}</p>
+//           </div>
+//           <div>
+//             <button onClick={() => handleChange(item, 1)}>+</button>
+//             <button>{item.amount}</button>
+//             <button onClick={() => handleChange(item, -1)}>-</button>
+//           </div>
+//           <div>
+//             <span>{item.price}</span>
+//             <button onClick={() => handleRemove(item.id)}>delete</button>
+//           </div>
+//         </div>
+//       ))}
+//       <div className="total">
+//         <span>Total Price of your Cart</span>
+//         <span>Total sum{price}</span>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Cart;
+
+
 import React, { useState, useEffect } from "react";
 import "../styles/cart.css";
 
@@ -40,7 +92,7 @@ const Cart = ({ cart, setCart, handleChange }) => {
         </div>
       ))}
       <div className="total">
-        <span>Total Price of your Cart</span>
+        <span>Total Price of your Shop</span>
         <span>Total sum{price}</span>
       </div>
     </article>
